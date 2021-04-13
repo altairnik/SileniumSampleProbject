1d93167a156b05b106d62cd608bbe56585124549
altairnik <altairnik@gmail.com>
2021-04-13 17:23:07 +0300

    fix webdriver

--- src/test/java/hillel/SimpleTest.java
+++ src/test/java/hillel/SimpleTest.java
@@ -1,3 +1,15 @@
 package hillel;
 
-public class SimpleTest {}
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    WebDriver driver;
+    @Test
+    public void checkMethod(){
+        System.setProperty("webdriver.chrome.driver", "c:/testing/chromedriver.exe");
+        driver  = new ChromeDriver();
+    driver.get("http://research.mawashi.tools:8080/");
+    }
+}
