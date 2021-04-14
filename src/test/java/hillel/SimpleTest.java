bf547f8573bd75b6c061a8e6cbab990a26cb5c69
altairnik <altairnik@gmail.com>
2021-04-14 14:41:07 +0300

    First Selenium has heen finished)

--- src/test/java/hillel/SimpleTest.java
+++ src/test/java/hillel/SimpleTest.java
@@ -1,5 +1,6 @@
 package hillel;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -11,5 +12,8 @@ public class SimpleTest {
         System.setProperty("webdriver.chrome.driver", "c:/testing/chromedriver.exe");
         driver  = new ChromeDriver();
     driver.get("http://research.mawashi.tools:8080/");
+    driver.findElement(By.cssSelector("input[name=\"username\"]")).sendKeys("altair");
+    driver.findElement(By.cssSelector("input[name=\"password\"]")).sendKeys("asdffdsA1");
+    driver.findElement(By.cssSelector("input.btn")).click();
     }
 }
